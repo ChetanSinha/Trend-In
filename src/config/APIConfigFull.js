7f918c919bdbc8b9530e526a0e6e1b0e6e982c48
Chetan Sinha <50909804+ChetanSinha@users.noreply.github.com>
2021-08-13 00:46:58 +0530

    Update APIConfigFull.js

--- src/config/APIConfigFull.js
+++ src/config/APIConfigFull.js
@@ -1,3 +1,3 @@
 export const articleURL = "http://newsapi.org/v2/everything";
 export const language = "en";
-export const API_KEY = "fcee0cb1524f46fc91976315ed3973c7";
+export const API_KEY = "YOUR_API_KEY";
